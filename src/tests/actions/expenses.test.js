b55ceec8607b23a2ae9a79283f3ebc2b94d811ee
Hongshen2010 <yuhongshen2010@gmail.com>
2017-11-15 13:33:36 -0500

    action generator test

---
+++ src/tests/actions/expenses.test.js
@@ -0,0 +1,50 @@
+import { addExpense, editExpense, removeExpense } from '../../actions/expenses';
+
+test('test REMOVE expense action', () => {
+    const id = '123';
+    const action = removeExpense(id);
+    expect(action).toEqual({
+        type: 'REMOVE_EXPENSE',
+        id
+    });
+});
+test('test EDIT expense action', () => {
+    const id = '123';
+    const action = editExpense(id, {amount: 100});
+    expect(action).toEqual({
+        type: 'EDIT_EXPENSE',
+        id,
+        updates: {
+            amount: 100
+        }
+    });
+});
+test('test ADD expense action', () => {
+    const expense = {
+        description: 'testDescription', 
+        note: 'testNote', 
+        amount: 1999, 
+        createdAt: 2999 
+    }
+    const action = addExpense(expense);
+    expect(action).toEqual({
+        type: 'ADD_EXPENSE',
+        expense: {
+            ...expense,
+            id: expect.any(String)
+        }
+    });
+});
+test('test ADD expense DEFAULT action', () => {
+    const action = addExpense();
+    expect(action).toEqual({
+        type: 'ADD_EXPENSE',
+        expense: {
+                    description: '',
+                    note: '',
+                    amount: 0,
+                    createdAt: 0,
+                    id: expect.any(String)
+                }
+    });
+});
--- src/tests/add.test.js
+++ src/tests/add.test.js
@@ -1,6 +1,7 @@
 const add = (a, b) => {
     return a + b;
 };
+const generateName = (name = 'Anonymous') => `${name}`;
 
 test('should add two numbers', () => {
     const result = add(3, 4);
@@ -8,4 +9,12 @@ test('should add two numbers', () => {
     // if(result !== 7) {
     //     throw new Error(`Added 4 and 3, the result was ${result}. Expect 7`);
     // }
+});
+test('should return a name', () => {
+    const result = generateName('hongshen');
+    expect(result).toBe('hongshen');
+});
+test('should return a name', () => {
+    const result = generateName();
+    expect(result).toBe('Anonymous');
 });
