3ce76c4917caeb2f71d8d98c6917d5d3b86c1f87
Hongshen2010 <yuhongshen2010@gmail.com>
2017-11-15 10:19:49 -0500

    testing

--- src/tests/add.test.js
+++ src/tests/add.test.js
@@ -5,4 +5,7 @@ const add = (a, b) => {
 test('should add two numbers', () => {
     const result = add(3, 4);
     expect(result).toBe(7);
+    // if(result !== 7) {
+    //     throw new Error(`Added 4 and 3, the result was ${result}. Expect 7`);
+    // }
 });
