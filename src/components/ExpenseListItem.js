9661e8b776b72104ddfead72b340b9452264485d
Hongshen2010 <yuhongshen2010@gmail.com>
2017-11-18 12:30:45 -0500

    component test and format display

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import moment from 'moment';
+import numeral from 'numeral';
 import { Link } from 'react-router-dom';
 import ExpenseList from './ExpenseList';
 import { connect } from 'react-redux';
@@ -8,14 +10,11 @@ const ExpenseListItem = ({ description, amount, createdAt, id }) => (
         <Link to={`/edit/${id}`}>
             <h3>{description}</h3>
         </Link>
-        <p>{amount} - {createdAt}</p>
+        <p>
+        {numeral(amount).format('$0,0.00')} 
+        - 
+        {moment(createdAt).format('MMMM Do, YYYY')}</p>
     </div>
 );
 
-const mapStateToPorps = (state) => {
-    return {
-        expenses: state.expenses
-    };
-};
-
-export default connect(mapStateToPorps)(ExpenseListItem);
+export default ExpenseListItem;
--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import ReactShallowRenderer from 'react-test-renderer/shallow';
 import { shallow } from 'enzyme';
 import Header from '../../components/Header';
 
@@ -8,5 +7,6 @@ test('Header rendering test', () => {
     // renderer.render(<Header />);
     // expect(renderer.getRenderOutput()).toMatchSnapshot();
     const wrapper = shallow(<Header />);
-    expect(wrapper.find('h1').text()).toBe('Expensify');
+    // expect(wrapper.find('h1').text()).toBe('Expense records');
+    expect(wrapper).toMatchSnapshot();
 });
