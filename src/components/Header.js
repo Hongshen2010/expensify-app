91cc0ab1522b60c9d98af0f5acc1da683b5a92e1
Hongshen2010 <yuhongshen2010@gmail.com>
2017-11-19 22:41:42 -0500

    add authentication system and private router

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import { BrowserRouter, NavLink } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { startLogOut } from '../actions/auth';
 
-const Header = () => (
+export const Header = ({ startLogOut }) => (
     <header>
         <h1>Expense records</h1>
         <NavLink exact to="/" activeClassName="is-active">Home </NavLink>
         <NavLink to="/create" activeClassName="is-active">create </NavLink>
         <NavLink to="/help" activeClassName="is-active">help</NavLink>
+        <button onClick={startLogOut}>Logout</button>
     </header>
 );
 
-export default Header;
+const mapDispatchToProps = (dispatch) => ({
+    startLogOut: () => dispatch(startLogOut())
+});
+
+export default connect(undefined, mapDispatchToProps)(Header);
