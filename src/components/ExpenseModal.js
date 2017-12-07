3aaf26e9c2af3d595d18346d3bfe64e5c9916996
Hongshen2010 <yuhongshen2010@gmail.com>
2017-12-07 18:49:27 -0500

    change expense modal to functional component

--- src/components/ExpenseModal.js
+++ src/components/ExpenseModal.js
@@ -3,41 +3,40 @@ import { connect } from 'react-redux';
 import Modal from 'react-modal';
 import { startRemoveExpense } from '../actions/expenses';
 
-class ExpenseModal extends React.Component {
-    render() {
-        const { description, id } = this.props.expense;
-        return (
-            <Modal
-                isOpen={!!this.props.selectedExpense}
-                contentLabel="Delete Expense?"
-                onRequestClose={this.props.clearSelectedExpense}
-                closeTimeoutMS={200}
-                ariaHideApp={false}
-                className="modal"
+const ExpenseModal = (props) => {
+    const { description, id } = props.expense;
+    return (
+        <Modal
+            isOpen={!!props.selectedExpense}
+            contentLabel="Delete Expense?"
+            onRequestClose={props.clearSelectedExpense}
+            closeTimeoutMS={200}
+            ariaHideApp={false}
+            className="modal"
+        >
+            <h3 className="modal__title">Delete the following Expense?</h3>
+            <h4 className="modal__body">{description}</h4>
+            <button
+                className="button"
+                onClick={() => {
+                    props.dispatch(startRemoveExpense(id));
+                    props.history.push('/');
+                }}
             >
-                <h3 className="modal__title">Delete the following Expense?</h3>
-                <h4 className="modal__body">{description}</h4>
-                <button
-                    className="button"
-                    onClick={() => {
-                        this.props.dispatch(startRemoveExpense(id));
-                        this.props.history.push('/');
-                    }}
-                >
-                    Yes, delete it.
-                </button>
-                <button
-                    className="button button--secondary"
-                    onClick={() => {
-                        this.props.history.push(this.props.match.url);
-                    }}
-                >
-                    No, keep it.
-                </button>
-            </Modal>
-        );
-    };
+                Yes, delete it.
+            </button>
+            <button
+                className="button button--secondary"
+                onClick={() => {
+                    props.history.push(props.match.url);
+                }}
+            >
+                No, keep it.
+            </button>
+        </Modal>
+    );
 }
+
 const mapStateToProps = (state, props) => ({
     expense: state.expenses.find((expense) => expense.id === props.expense.match.params.id)
 });
